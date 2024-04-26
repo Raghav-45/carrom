23506b4df9d421086ebcb1c4e6429c0244c446e9
Aditya <raghavbhai4545@gmail.com>
2024-04-26 21:59:42 +0530

    Update striker.cs

--- Assets/striker.cs
+++ Assets/striker.cs
@@ -7,10 +7,9 @@ public class striker : MonoBehaviour
 {
   Vector3 strikerPosition, endPos, targetDirection, touchPosition;
 
-  // public float forceMultiplier = 350f;
   public float forceMultiplier = 1500f;
-  public float minRequiredForce = 3f;
-  public float maxForce = 5f;
+  public float minRequiredForce = 0.8f;
+  public float maxForce = 6.4f;
 
   // TODO: Instea of Array, do something else
   public AudioSource[] breakshots;
@@ -71,92 +70,13 @@ public class striker : MonoBehaviour
     // player = t;
   }
   private void control()
-  // {
-  //   if (Input.touchCount > 0)
-  //   {
-  //     startPos = this.transform.position;
-
-  //     lr.positionCount = 2;
-  //     lr.widthCurve = ac;
-  //     lr.numCapVertices = 10;
-  //     lr.enabled = false;
-  //     lr.SetPosition(0, startPos);
-
-  //     if (Input.GetMouseButton(0) || (Input.GetTouch(0).phase == TouchPhase.Began))
-  //     // {
-  //     //   endPos = cm.ScreenToWorldPoint(Input.mousePosition);
-  //     //   lr.SetPosition(1, endPos);
-
-  //     //   lr.enabled = true;
-  //     // }
-  //     {
-  //       endPos = cm.ScreenToWorldPoint(Input.mousePosition);
-
-  //       // Calculate the direction from start position to input position
-  //       Vector3 hitDirection = startPos - endPos;
-
-  //       // Calculate the end position
-  //       Vector3 linePos = startPos + hitDirection;
-
-  //       // Draw a debug line from startPos to endPos
-  //       Debug.DrawLine(startPos, endPos, Color.blue);
-
-  //       // Set the end position for the line renderer
-  //       lr.SetPosition(1, linePos);
-
-  //       // Enable the line renderer
-  //       lr.enabled = true;
-  //     }
-
-  //     if (Input.GetMouseButtonUp(0) || (Input.GetTouch(0).phase == TouchPhase.Ended))
-  //     {
-  //       lr.enabled = false;
-  //       // hit = true;
-  //       st = true;
-  //       targetDirection = endPos - startPos;
-  //       Vector3 testtargetDirection = startPos - endPos;
-
-  //       // Draw a debug line to visualize the target direction
-  //       Debug.DrawLine(startPos, endPos, Color.green, 2f);
-
-  //       if (targetDirection.magnitude > 10.007f)
-  //       {
-  //         lr.material.color = Color.red;
-
-  //         breakshots[2].clip = hitsound[Random.Range(0, hitsound.Length)];
-  //         breakshots[2].Play();
-
-  //         Debug.Log(testtargetDirection.magnitude);
-
-  //         // 10 > is good
-  //         rb.AddForce(testtargetDirection * 350);
-
-  //         player = false; // Set this to False to Make Player One Always Take Turn
-
-  //         // if (player == true && this.GetComponent<pocket>().get_st() == false)
-  //         // {
-  //         //   player = false;
-  //         // }
-  //         // else if (player == false && this.GetComponent<pocket>().get_st() == false)
-  //         // {
-  //         //   player = true;
-  //         // }
-  //       }
-  //     }
-
-  //     breakshots[3].clip = movesound[Random.Range(0, movesound.Length)];
-  //     breakshots[3].volume = Mathf.Clamp01(rb.velocity.sqrMagnitude / 200);
-  //     breakshots[3].Play();
-  //   }
-  // }
   {
-    // Vector3 touchPosition;
     if (Input.touchCount > 0)
     {
       Touch touch = Input.GetTouch(0);
       touchPosition = cm.ScreenToWorldPoint(Input.mousePosition);
 
-      // Raycast to check if the touch position is hitting the board collider
+      // Raycast to check if the touch position is hitting the striker collider
       RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector3.forward);
 
       if (hit.collider)
@@ -170,7 +90,6 @@ public class striker : MonoBehaviour
         if (showGizmos == true)
         {
           strikerPosition = this.transform.position;
-          // powerCircle.transform.LookAt(hit.point);
           float scaleValue = 4f * Vector2.Distance(strikerPosition, touchPosition);
           // powerCircle.transform.localScale = Vector3.one * Mathf.Clamp(scaleValue, 0f, maxForce);
           powerCircle.transform.localScale = Vector3.one * scaleValue;
@@ -178,11 +97,6 @@ public class striker : MonoBehaviour
           // Draw Debug line for Force Vector
           Debug.DrawLine(strikerPosition, touchPosition, Color.blue);
 
-          // Calculate the direction from start position to input position
-          // Vector3 hitDirection = strikerPosition - touchPosition;
-
-          // // Calculate the end position
-          // Vector3 linePos = strikerPosition + hitDirection;
           // Calculate the direction from start position to input position
           targetDirection = strikerPosition - touchPosition;
 
@@ -200,23 +114,20 @@ public class striker : MonoBehaviour
     }
     else if (Input.touchCount == 0 && showGizmos == true)
     {
+      lr.enabled = false;
       showGizmos = false;
       powerCircle.transform.localScale = Vector3.zero;
-
-      lr.enabled = false;
 
       if (targetDirection.magnitude > 10f)
       {
         breakshots[2].clip = hitsound[Random.Range(0, hitsound.Length)];
         breakshots[2].Play();
 
-
         float magnitude = 4f * Vector2.Distance(strikerPosition, touchPosition);
         // Vector3 forceVector = targetDirection.normalized * Mathf.Clamp(magnitude, 0f, maxForce);
         Vector3 forceVector = targetDirection.normalized * magnitude;
         Debug.Log(forceVector.magnitude);
 
-        // 10 > is good
         rb.AddForce(forceVector * forceMultiplier);
 
         player = false; // Set this to False to Make Player One Always Take Turn
