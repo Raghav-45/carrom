b650aac6ad7b4bbcea03b6d2c8626f42d2149fb0
Aditya <raghavbhai4545@gmail.com>
2024-04-27 15:44:35 +0530

    fixed controls

--- Assets/striker.cs
+++ Assets/striker.cs
@@ -1,170 +1,173 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class striker : MonoBehaviour
 {
-  Vector3 strikerPosition, endPos, targetDirection, touchPosition;
+    Vector3 strikerPosition, endPos, targetDirection, touchPosition;
 
-  public float forceMultiplier = 1200f;
-  public float minRequiredForce = 0.8f;
-  public float maxForce = 4f;
+    public float forceMultiplier = 200f;
+    public float minRequiredForce = 0.8f;
+    public float maxForce = 4f;
 
-  // TODO: Instea of Array, do something else
-  public AudioSource[] breakshots;
-  public AudioClip[] hitsound, breaksound, hits, pocketfillsound, movesound;
+    // TODO: Instea of Array, do something else
+    public AudioSource[] breakshots;
+    public AudioClip[] hitsound, breaksound, hits, pocketfillsound, movesound;
 
-  [SerializeField] Slider StrikerSlider;
+    [SerializeField] Slider StrikerSlider;
 
-  public GameObject powerCircle;
-  public float resetThresholdVelocity = 0.07f;
-  public GameObject board;
-  bool player = false;
-  public int black, white, red;
-  public bool coveringTheQueen = false;
+    public GameObject powerCircle;
+    public float resetThresholdVelocity = 0.07f;
+    public GameObject board;
+    bool player = false;
+    public int black, white, red;
+    public bool coveringTheQueen = false;
 
-  Camera cm;
-  LineRenderer lr;
-  GameObject start;
+    Camera cm;
+    LineRenderer lr;
+    GameObject start;
 
-  Rigidbody2D rb;
-  // bool hit = false;
-  public bool st = false;
-  public bool movestriker = false;
-  public bool turn = false;
-  bool showGizmos = false;
-  [SerializeField] AnimationCurve ac;
+    Rigidbody2D rb;
+    // bool hit = false;
+    public bool st = false;
+    public bool movestriker = false;
+    public bool turn = false;
+    bool showGizmos = false;
+    [SerializeField] AnimationCurve ac;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    start = GameObject.Find("start");
-
-    cm = Camera.main;
-    lr = GetComponent<LineRenderer>();
-    rb = GetComponent<Rigidbody2D>();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (rb.velocity.magnitude <= resetThresholdVelocity)
+    // Start is called before the first frame update
+    void Start()
     {
-      ReturnStriker(); // Reset Striker Position
-      if (movestriker == false)
-      {
-        breakshots[3].Stop();
-        control();
-      }
+        start = GameObject.Find("start");
+
+        cm = Camera.main;
+        lr = GetComponent<LineRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-  }
-  public void MoveStriker(bool t)
-  {
-    if (rb.velocity.magnitude <= resetThresholdVelocity)
+    // Update is called once per frame
+    void Update()
     {
-      float yPosition = t ? 1.47f : -1.47f;
-      this.transform.position = new Vector3(StrikerSlider.value, yPosition, 0);
-    }
-    // player = t;
-  }
-  private void control()
-  {
-    if (Input.touchCount > 0)
-    {
-      Touch touch = Input.GetTouch(0);
-      touchPosition = cm.ScreenToWorldPoint(Input.mousePosition);
-
-      // Raycast to check if the touch position is hitting the striker collider
-      RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector3.forward);
-
-      if (hit.collider)
-      {
-        if (hit.transform.name == "striker")
+        if (rb.velocity.magnitude <= resetThresholdVelocity)
         {
-          // Debug.Log("Touch on striker");
-          showGizmos = true;
+            ReturnStriker(); // Reset Striker Position
+            if (movestriker == false)
+            {
+                breakshots[3].Stop();
+                control();
+            }
         }
 
-        if (showGizmos == true)
+    }
+    public void MoveStriker(bool t)
+    {
+        if (rb.velocity.magnitude <= resetThresholdVelocity)
         {
-          strikerPosition = this.transform.position;
-          float scaleValue = 4f * Vector2.Distance(strikerPosition, touchPosition);
-          // powerCircle.transform.localScale = Vector3.one * Mathf.Clamp(scaleValue, 0f, maxForce);
-          powerCircle.transform.localScale = Vector3.one * Mathf.Clamp(scaleValue, 0f, maxForce);
-
-          // Draw Debug line for Force Vector
-          Debug.DrawLine(strikerPosition, touchPosition, Color.blue);
-
-          // Calculate the direction from start position to input position
-          targetDirection = strikerPosition - touchPosition;
-
-          // Calculate the end position
-          Vector3 linePos = strikerPosition + targetDirection;
-
-          lr.positionCount = 2;
-          lr.widthCurve = ac;
-          lr.numCapVertices = 10;
-          lr.SetPosition(0, strikerPosition);
-          lr.SetPosition(1, linePos);
-          lr.enabled = true;
+            float yPosition = t ? 1.47f : -1.47f;
+            this.transform.position = new Vector3(StrikerSlider.value, yPosition, 0);
         }
-      }
+        // player = t;
     }
-    else if (Input.touchCount == 0 && showGizmos == true)
+    private void control()
     {
-      lr.enabled = false;
-      showGizmos = false;
-      powerCircle.transform.localScale = Vector3.zero;
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = cm.ScreenToWorldPoint(Input.mousePosition);
 
-      if (targetDirection.magnitude > 10f)
-      {
-        breakshots[2].clip = hitsound[Random.Range(0, hitsound.Length)];
-        breakshots[2].Play();
+            // Raycast to check if the touch position is hitting the striker collider
+            RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector3.forward);
 
-        float magnitude = 4f * Vector2.Distance(strikerPosition, touchPosition);
-        // Vector3 forceVector = targetDirection.normalized * magnitude;
-        Vector3 forceVector = targetDirection.normalized * Mathf.Clamp(magnitude, 0f, maxForce);
-        Debug.Log(forceVector.magnitude * forceMultiplier);
+            if (hit.collider)
+            {
+                if (hit.transform.name == "striker")
+                {
+                    // Debug.Log("Touch on striker");
+                    showGizmos = true;
+                }
 
-        rb.AddForce(forceVector * forceMultiplier);
+                if (showGizmos == true)
+                {
+                    strikerPosition = this.transform.position;
+                    float scaleValue = 4f * Vector2.Distance(strikerPosition, touchPosition);
+                    // powerCircle.transform.localScale = Vector3.one * Mathf.Clamp(scaleValue, 0f, maxForce);
+                    powerCircle.transform.localScale = Vector3.one * Mathf.Clamp(scaleValue, 0f, maxForce);
 
-        player = false; // Set this to False to Make Player One Always Take Turn
-      }
+                    // Draw Debug line for Force Vector
+                    Debug.DrawLine(strikerPosition, touchPosition, Color.blue);
+
+                    // Calculate the direction from start position to input position
+                    targetDirection = strikerPosition - touchPosition;
+
+                    // Calculate the end position
+                    // Vector3 linePos = strikerPosition + targetDirection;
+
+                    // lr.positionCount = 2;
+                    // lr.widthCurve = ac;
+                    // lr.numCapVertices = 10;
+                    // lr.SetPosition(0, strikerPosition);
+                    // lr.SetPosition(1, linePos);
+                    // lr.enabled = true;
+                }
+            }
+        }
+        else if (Input.touchCount == 0 && showGizmos == true)
+        {
+            // lr.enabled = false;
+            showGizmos = false;
+            powerCircle.transform.localScale = Vector3.zero;
+
+            Vector2 hitDirectionNormalized = targetDirection.normalized;
+            float dragAmount = 4f * Vector2.Distance(strikerPosition, touchPosition);
+            float magnitudeClamped = Mathf.Clamp(dragAmount, 0f, maxForce);
+            // Vector3 forceVector = targetDirection.normalized * magnitude;
+            Vector3 forceVector = hitDirectionNormalized.normalized * magnitudeClamped;
+
+            if (magnitudeClamped > 0.9f)
+            {
+                Debug.Log(forceVector.magnitude * forceMultiplier);
+                breakshots[2].clip = hitsound[Random.Range(0, hitsound.Length)];
+                breakshots[2].Play();
+
+                rb.AddForce(forceVector.normalized * magnitudeClamped * forceMultiplier);
+
+                player = false; // Set this to False to Make Player One Always Take Turn
+            }
+        }
     }
-  }
-  public void ReturnStriker()
-  {
-    if (player == false)
+    public void ReturnStriker()
     {
-      MoveStriker(false);
+        if (player == false)
+        {
+            MoveStriker(false);
+        }
+        else
+        {
+            MoveStriker(true);
+        }
     }
-    else
-    {
-      MoveStriker(true);
-    }
-  }
 
-  private void OnCollisionEnter2D(Collision2D other)
-  {
-    if (other.gameObject.tag == "black" || other.gameObject.tag == "white" || other.gameObject.tag == "red")
+    private void OnCollisionEnter2D(Collision2D other)
     {
-      if (board.GetComponent<Collider2D>().enabled == false)
-      {
-        breakshots[1].clip = hits[Random.Range(0, hits.Length)];
-        breakshots[1].volume = Mathf.Clamp01(other.relativeVelocity.magnitude / 30);
-        breakshots[1].Play();
-      }
+        if (other.gameObject.tag == "black" || other.gameObject.tag == "white" || other.gameObject.tag == "red")
+        {
+            if (board.GetComponent<Collider2D>().enabled == false)
+            {
+                breakshots[1].clip = hits[Random.Range(0, hits.Length)];
+                breakshots[1].volume = Mathf.Clamp01(other.relativeVelocity.magnitude / 30);
+                breakshots[1].Play();
+            }
+        }
     }
-  }
-  void OnTriggerEnter2D(Collider2D other)
-  {
-    if (other.gameObject.tag == "board")
+    void OnTriggerEnter2D(Collider2D other)
     {
-      breakshots[0].volume = Mathf.Clamp01(rb.velocity.sqrMagnitude / 200);
-      board.GetComponent<Collider2D>().enabled = false;
-      breakshots[0].Play();
+        if (other.gameObject.tag == "board")
+        {
+            breakshots[0].volume = Mathf.Clamp01(rb.velocity.sqrMagnitude / 200);
+            board.GetComponent<Collider2D>().enabled = false;
+            breakshots[0].Play();
+        }
     }
-  }
 }
