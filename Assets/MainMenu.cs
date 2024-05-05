0ec271be37c8aa32137674dec82c42b20626956b
Aditya <raghavbhai4545@gmail.com>
2024-05-05 19:28:22 +0530

    Update MainMenu.cs

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("ShadowTwo");
+        SceneManager.LoadScene("ShadowTwoPlayers");
     }
 
     public void QuitGame()
