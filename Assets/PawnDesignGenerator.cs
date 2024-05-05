b743c0f3ac592680779d4295d696ae00eae3c095
Aditya <raghavbhai4545@gmail.com>
2024-05-05 21:14:15 +0530

    Update PawnDesignGenerator.cs

--- Assets/PawnDesignGenerator.cs
+++ Assets/PawnDesignGenerator.cs
@@ -19,7 +19,7 @@ public class PawnDesignGenerator : MonoBehaviour
         { new Vector2(-0.3810512f, -0.22f), Color.white }, // White
         { new Vector2(0f, 0.44f), Color.white }, // White
         { new Vector2(0.3810512f, -0.22f), Color.white }, // White
-        
+
         { new Vector2(0f, 0.22f), Color.black }, // Black
         { new Vector2(0.1905256f, -0.11f), Color.black }, // Black
         { new Vector2(-0.1905256f, -0.11f), Color.black }, // Black
@@ -33,20 +33,21 @@ public class PawnDesignGenerator : MonoBehaviour
 
     void Start()
     {
-        SpawnCirclesAtCoordinates();
+        // SpawnCirclesAtCoordinates();
+        this.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(-9f, 18f));
     }
 
-    void OnDrawGizmos()
-    {
-        foreach (var kvp in coordinateColors)
-        {
-            Vector2 position = kvp.Key;
-            Color color = kvp.Value;
+    // void OnDrawGizmos()
+    // {
+    //     foreach (var kvp in coordinateColors)
+    //     {
+    //         Vector2 position = kvp.Key;
+    //         Color color = kvp.Value;
 
-            Gizmos.color = color;
-            Gizmos.DrawSphere(new Vector3(position.x, position.y, 0), 0.1f);
-        }
-    }
+    //         Gizmos.color = color;
+    //         Gizmos.DrawSphere(new Vector3(position.x, position.y, 0), 0.1f);
+    //     }
+    // }
 
     void SpawnCirclesAtCoordinates()
     {
