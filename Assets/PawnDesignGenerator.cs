d4620478cef079012dfb46b97fb6818f5c19e4a6
Aditya <raghavbhai4545@gmail.com>
2024-05-05 20:46:34 +0530

    Update PawnDesignGenerator.cs

--- Assets/PawnDesignGenerator.cs
+++ Assets/PawnDesignGenerator.cs
@@ -2,22 +2,46 @@ using UnityEngine;
 
 public class PawnDesignGenerator : MonoBehaviour
 {
-    public GameObject circlePrefab; // Reference to the GameObject you want to spawn
+    [SerializeField] GameObject circlePrefab; // Reference to the GameObject you want to spawn
 
     // Coordinates of circle centers
-    private Vector2[] circleCoordinates = new Vector2[]
+    Vector2[] circleCoordinates = new Vector2[]
     {
-        new Vector2(0.39f, 0.00f),
-        new Vector2(0.20f, 0.34f),
-        new Vector2(-0.20f, 0.34f),
-        new Vector2(-0.39f, 0.00f),
-        new Vector2(-0.20f, -0.34f),
-        new Vector2(0.20f, -0.34f)
+        // new Vector2(0f, 0f), // Red
+        // new Vector2(-0.1905256f, 0.11f), // White
+        // new Vector2(0.1905256f, 0.11f), // White
+        // new Vector2(0f, -0.22f), // White
+        // new Vector2(0f, -0.44f), // White
+        // new Vector2(0.3810512f, 0.22f), // White
+        // new Vector2(-0.3810512f, 0.22f), // White
+        // new Vector2(-0.3810512f, -0.22f), // White
+        // new Vector2(0f, 0.44f), // White
+        // new Vector2(0.3810512f, -0.22f), // White
+        new Vector2(0f, 0.22f), // Black
+        new Vector2(0.1905256f, -0.11f), // Black
+        new Vector2(-0.1905256f, -0.11f), // Black
+        new Vector2(0.1905256f, -0.33f), // Black
+        new Vector2(-0.1905256f, -0.33f), // Black
+        new Vector2(0.1905256f, 0.33f), // Black
+        new Vector2(0.3810512f, 0f), // Black
+        new Vector2(-0.3810512f, 0f), // Black
+        new Vector2(-0.1905256f, 0.33f) // Black
     };
 
     void Start()
     {
         SpawnCirclesAtCoordinates();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        // Draw a small sphere at each circle spawn point
+        foreach (Vector2 position in circleCoordinates)
+        {
+            Gizmos.DrawSphere(new Vector3(position.x, position.y, 0), 0.1f);
+        }
     }
 
     void SpawnCirclesAtCoordinates()
