bb90e790a56902b78e984c671e1af9a04eb9bff0
Aditya <raghavbhai4545@gmail.com>
2024-05-05 20:21:06 +0530

    Update pocket.cs

--- Assets/pocket.cs
+++ Assets/pocket.cs
@@ -3,15 +3,8 @@ using UnityEngine;
 
 public class pocket : MonoBehaviour
 {
-    GameObject gameManager;
     Animator anim;
     Rigidbody2D rb;
-
-    // Awake is called when the script instance is being loaded
-    private void Awake()
-    {
-        gameManager = GameObject.Find("Game Manager");
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -22,123 +15,34 @@ public class pocket : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        // // Object is Striker
-        // if (other.gameObject.tag == "pocket" && this.gameObject.tag == "striker")
-        // {
-        //     rb.velocity = Vector2.zero;
-        //     StartCoroutine("fall");
-        //     if (this.gameObject.GetComponent<striker>().coveringTheQueen == true)
-        //     {
-        //         //TODO: return the queen
-        //         this.gameObject.GetComponent<striker>().coveringTheQueen = false;
-        //     }
-        // }
-
-        // // Object is Coin
-        // if (other.gameObject.tag == "pocket" && (this.gameObject.tag == "black" || this.gameObject.tag == "red" || this.gameObject.tag == "white"))
-        // {
-        //     // Give one Bonus Turn
-        //     gameManager.GetComponent<Game_Manager>().giveTurn();
-
-        //     if (this.gameObject.tag == "black")
-        //     {
-        //         GetCurrentPlayingCharacter().GetComponent<striker>().black++;
-        //         GetCurrentPlayingCharacter().GetComponent<striker>().UpdateScore();
-        //         if (GetCurrentPlayingCharacter().GetComponent<striker>().coveringTheQueen == true)
-        //         {
-        //             //TODO: also can add red++ instead of setting it to 1 ( for freeplay mode )
-        //             GetCurrentPlayingCharacter().GetComponent<striker>().red = 1;
-        //             GetCurrentPlayingCharacter().GetComponent<striker>().coveringTheQueen = false;
-        //         }
-        //     }
-        //     if (this.gameObject.tag == "red")
-        //     {
-        //         // striker.GetComponent<striker>().red++;
-        //         GetCurrentPlayingCharacter().GetComponent<striker>().coveringTheQueen = true;
-        //     }
-        //     if (this.gameObject.tag == "white")
-        //     {
-        //         GetCurrentPlayingCharacter().GetComponent<striker>().white++;
-        //         GetCurrentPlayingCharacter().GetComponent<striker>().UpdateScore();
-        //         if (GetCurrentPlayingCharacter().GetComponent<striker>().coveringTheQueen == true)
-        //         {
-        //             //TODO: also can add red++ instead of setting it to 1 ( for freeplay mode )
-        //             GetCurrentPlayingCharacter().GetComponent<striker>().red = 1;
-        //             GetCurrentPlayingCharacter().GetComponent<striker>().coveringTheQueen = false;
-        //         }
-        //     }
-
-        //     rb.velocity = Vector2.zero;
-        //     anim.SetTrigger("fall");
-        //     Destroy(this.gameObject);
-        // }
-
         if (other.gameObject.tag != "pocket") return;
 
         switch (this.gameObject.tag)
         {
             case "striker":
                 rb.velocity = Vector2.zero;
-                // StartCoroutine("fall");
-                // if (this.gameObject.GetComponent<striker>().coveringTheQueen == true)
-                // {
-                //     //TODO: return the queen
-                //     this.gameObject.GetComponent<striker>().coveringTheQueen = false;
-                // }
                 break;
             case "black":
+            case "red":
             case "white":
-                // Give one Bonus Turn
-                // gameManager.GetComponent<Game_Manager>().giveTurn();
+                // TODO: Give one Bonus Turn
 
-                if (this.gameObject.tag == "black")
+                switch (this.gameObject.tag)
                 {
-                    // GetStriker().black++;
-                    // gameManager.GetComponent<Game_Manager>().deltaCoins++;
-
-                    // Collect the coin using the GameManager
-                    GameManager.Instance.AddCoinToCurrentPlayer(CoinType.Black);
-                }
-                else if (this.gameObject.tag == "white")
-                {
-                    // GetStriker().white++;
-                    // gameManager.GetComponent<Game_Manager>().deltaCoins++;
-
-                    // Collect the coin using the GameManager
-                    GameManager.Instance.AddCoinToCurrentPlayer(CoinType.White);
+                    case "black":
+                        GameManager.Instance.AddCoinToCurrentPlayer(CoinType.Black);
+                        break;
+                    case "red":
+                        GameManager.Instance.AddCoinToCurrentPlayer(CoinType.Red);
+                        break;
+                    case "white":
+                        GameManager.Instance.AddCoinToCurrentPlayer(CoinType.White);
+                        break;
                 }
 
                 GameManager.Instance.UpdateScoreUI();
 
-                // if (GetStriker().coveringTheQueen)
-                // {
-                //     //TODO: also can add red++ instead of setting it to 1 ( for freeplay mode )
-                //     GetStriker().red = 1;
-                //     GetStriker().coveringTheQueen = false;
-                // }
-
                 rb.velocity = Vector2.zero;
-                // anim.SetTrigger("fall");
-                Destroy(this.gameObject);
-
-                break;
-            case "red":
-                // Give one Bonus Turn
-                // gameManager.GetComponent<Game_Manager>().giveTurn();
-                if (this.gameObject.tag == "red")
-                {
-                    // GetStriker().red++;
-                    // gameManager.GetComponent<Game_Manager>().deltaCoins++;
-
-                    // Collect the coin using the GameManager
-                    // GameManager.Instance.CollectCoin(CoinType.Red);
-                    GameManager.Instance.AddCoinToCurrentPlayer(CoinType.Red);
-                }
-
-                // GetStriker().coveringTheQueen = true;
-
-                rb.velocity = Vector2.zero;
-                // anim.SetTrigger("fall");
                 Destroy(this.gameObject);
 
                 break;
